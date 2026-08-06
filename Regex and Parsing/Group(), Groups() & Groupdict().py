511badda2c136669fb4717ff3bf6acb1063acbda
Aditya <Adityapurviya67@gmail.com>
2026-08-06 04:46:16 +0000

    compl

---
+++ Regex and Parsing/Group(), Groups() & Groupdict().py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+s = input()
+m = re.search(r'([A-Za-z0-9])\1+',s)
+
+if(m):
+    print(m.group(1))
+else:
+    print(-1)

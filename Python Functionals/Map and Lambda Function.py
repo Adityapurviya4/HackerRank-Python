695dc3b4d2e0c1460ca09d673de2d9f212e61df5
Aditya <Adityapurviya67@gmail.com>
2026-08-09 15:59:36 +0000

    You have earned 20.00 points! 32/115 challenges solved. python gold

---
+++ Python Functionals/Map and Lambda Function.py
@@ -0,0 +1,13 @@
+cube = lambda x: x **3 # complete the lambda function 
+
+def fibonacci(n):
+    # return a list of fibonacci numbers
+     f = [0,1]
+     for i in range(2,n):
+        f.append(f[i-1]+f[i-2])
+     return f[:n]
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
